86f48ad33bbe568582f1451ae32b2dbe69119d78
Klaire Tan <klaire.tan@yale.edu>
2018-03-07 14:29:21 -0800

    try

--- src/GraphqlCodeBlock.jsx
+++ src/GraphqlCodeBlock.jsx
@@ -17,36 +17,35 @@ export default class GraphqlCodeBlock extends Component {
 
     try {
       const formatted = print(parse(queryBody));
-      
-          const highlighted = [];
-          const rowKeys = [];
-          runParser(formatted, {
-            eatWhitespace: stream => stream.eatWhile(isIgnored),
-            LexRules,
-            ParseRules,
-          }, (stream, state, style) => {
-            const { _sourceText, _start, _pos } = stream;
-            if (rowKeys[rowKeys.length-1] !== _sourceText) {
-              rowKeys.push(_sourceText);
-              highlighted.push([]);
-            }
-            const substr = _sourceText.substring(_start, _pos);
-            highlighted[highlighted.length-1].push(
-              <span key={`${rowKeys.length}-${_start}-${_pos}`} className={style}>{substr}</span>
-            );
-          });
-      
-          const body = [];
-          highlighted.forEach((row, index) => {
-            body.push(<pre key={`query-row-${index}`}>{row}</pre>);
-          });
-      
-          return (
-            <div className={className}>{body}</div>
-          );
     } catch {
       return (<div className={className}>Could not parse graphQL query</div>);
     }
-    
+      
+    const highlighted = [];
+    const rowKeys = [];
+    runParser(formatted, {
+      eatWhitespace: stream => stream.eatWhile(isIgnored),
+      LexRules,
+      ParseRules,
+    }, (stream, state, style) => {
+      const { _sourceText, _start, _pos } = stream;
+      if (rowKeys[rowKeys.length-1] !== _sourceText) {
+        rowKeys.push(_sourceText);
+        highlighted.push([]);
+      }
+      const substr = _sourceText.substring(_start, _pos);
+      highlighted[highlighted.length-1].push(
+        <span key={`${rowKeys.length}-${_start}-${_pos}`} className={style}>{substr}</span>
+      );
+    });
+
+    const body = [];
+    highlighted.forEach((row, index) => {
+      body.push(<pre key={`query-row-${index}`}>{row}</pre>);
+    });
+
+    return (
+      <div className={className}>{body}</div>
+    );
   }
 }
